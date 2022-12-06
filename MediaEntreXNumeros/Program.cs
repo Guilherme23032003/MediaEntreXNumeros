dd9285c59ad9b2e67636b08fe1d81732da817d46
Guilherme Alexandre Marin <guiamarin@hotmail.com>
2022-12-05 21:33:04 -0300

    Adicionar arquivos de projeto.

---
+++ MediaEntreXNumeros/Program.cs
@@ -0,0 +1,14 @@
+﻿double numero1, numero2, resultado;
+
+Console.Write("informe o primeiro numero: ");
+
+numero1 = double.Parse(Console.ReadLine());
+
+Console.Write("agora, informe o segundo numero da equação: ");
+
+numero2= double.Parse(Console.ReadLine()); 
+
+resultado = (numero2 + numero1)/2;
+Console.Write("\n\n\na media dos numeros é: " + resultado);
+
+
